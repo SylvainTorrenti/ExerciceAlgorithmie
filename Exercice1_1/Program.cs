85e3eb01126392b8ea1506915e9476b905d8f711
SylvainTorrenti <torrsyl@gmail.com>
2022-11-29 15:08:05 +0100

    ajout exercice

---
+++ Exercice1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre nom:");
+            string nom = Console.ReadLine();
+            Console.WriteLine("bonjour " + nom);
+
+            Console.ReadLine();
+        }
+    }
+}
