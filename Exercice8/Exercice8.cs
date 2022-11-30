e4cd2f6e95535b27e3fa630d743c7a1393d3d1f3
SylvainTorrenti <torrsyl@gmail.com>
2022-11-30 10:55:57 +0100

    exercice

--- Exercice8/Exercice8.cs
+++ Exercice8/Exercice8.cs
@@ -11,17 +11,17 @@ namespace Exercice8
         static void Main(string[] args)
         {
             Console.WriteLine("Entrez un entier");
-            string Entier = Console.ReadLine();
-            int EntierConvertit = int.Parse(Entier);
-            if(EntierConvertit > 0 )
+            string Entier = Console.ReadLine(); //Stockage de l'entrée dans la console
+            int EntierConvertit = int.Parse(Entier); //Convertion en Int
+            if(EntierConvertit > 0 ) //si l'entier est > 0
             {
                 Console.WriteLine("Le nombre est positif");
             }
-            if (EntierConvertit < 0)
+            if (EntierConvertit < 0) //si l'entier est < 0
             {
                 Console.WriteLine("Le nombre est négatif");
             }
-            if (EntierConvertit == 0)
+            if (EntierConvertit == 0) //si l'entier est = 0
             {
                 Console.WriteLine("Le nombre est 0");
             }
---
+++ Exercice9/Exercice9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice9
+{
+    internal class Exercice9
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre note entre 0 et 20");
+            string Note = Console.ReadLine(); // Stock la note entrée
+            int NoteConvertit = int.Parse(Note); // Convertit la note en Int
+            if (NoteConvertit >= 10) //  si la note est supérieure ou égale à 10
+            {
+                Console.WriteLine("Admis");
+            }
+            if (NoteConvertit > 8 && NoteConvertit < 10) // si la note est supérieure à 8 et inférieur à 10
+            {
+                Console.WriteLine("Rattrapage");
+            }
+            if (NoteConvertit < 8)
+            {
+                Console.WriteLine("Echec"); //  si la note est inférieure à 8
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
