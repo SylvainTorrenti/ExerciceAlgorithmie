0217923228f9a58304a46f7d3b3721db3375a3c5
SylvainTorrenti <torrsyl@gmail.com>
2022-11-30 13:24:13 +0100

    exercice

---
+++ Exercice10/Exercice10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice10
+{
+    internal class Exercice10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ecrire un nombre");
+            string Nombre1 = Console.ReadLine();
+            Console.WriteLine("Ecrire un second nombre");
+            string Nombre2 = Console.ReadLine();
+            int Nombre1Convertit = int.Parse(Nombre1);
+            int Nombre2Convertit = int.Parse(Nombre2);
+
+            if (Nombre1Convertit < 0)
+            {
+                
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Exercice11/Exercice11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice11
+{
+    internal class Exercice11
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ecrivez un caractére");
+            string caractere = Console.ReadLine();
+            if ((caractere == "o") || (caractere == "O"))
+            {
+                Console.WriteLine("affirmatif");
+
+            }
+            if ((caractere == "n") || (caractere == "N"))
+            {
+                Console.WriteLine("négatif");
+            }
+            
+
+                Console.ReadLine();
+        }
+    }
+}
