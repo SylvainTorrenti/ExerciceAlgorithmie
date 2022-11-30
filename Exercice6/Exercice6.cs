c267a39e1e979b5e42814ac127c129b9f7398155
SylvainTorrenti <torrsyl@gmail.com>
2022-11-30 09:55:41 +0100

    exercice

--- Exercice6/Exercice6.cs
+++ Exercice6/Exercice6.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Exercice6
 {
-    internal class Program
+    internal class Exercice6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entrez un nombre");
+            string Nombre = Console.ReadLine(); // stockage du nombre en string
+            int NombreEntier = int.Parse(Nombre); // transformation du type string en int
+            int modulo = NombreEntier % 2; // Calcul du modulo
+            if (modulo == 0) // si le modulo = 0 le chiffre et pair
+            {
+                Console.WriteLine("Le nombre est pair");
+            }
+            else // si le modulo != 0 le chiffre et pair
+            {
+                Console.WriteLine("Le nombre est impair");
+            }
+            Console.ReadLine();
         }
     }
 }
