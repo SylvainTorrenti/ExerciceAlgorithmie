aa7fe7d36029ff9bc1fda7447ba5040a97bfece7
SylvainTorrenti <torrsyl@gmail.com>
2022-12-01 09:59:27 +0100

    Exercice

--- Exercice14/Exercice14.cs
+++ Exercice14/Exercice14.cs
@@ -13,6 +13,33 @@ namespace Exercice14
             Console.WriteLine("Entré le nombre de photocopie faites");
             string Photocopie = Console.ReadLine();
             int PhotocopieConvertit = int.Parse(Photocopie);
+            if (PhotocopieConvertit <= 10)
+            {
+                double PremierPrix = PhotocopieConvertit * 0.1;
+                Console.WriteLine("la facture est de " + PremierPrix);
+
+            }
+
+
+            if ((PhotocopieConvertit > 10) && (PhotocopieConvertit <= 20))
+            {
+                int PhotocopieDeux = PhotocopieConvertit - 10;
+                double DeuxiemePrix = (PhotocopieDeux * 0.09) + 1;
+                Console.WriteLine("La facture est de " + DeuxiemePrix);
+            }
+            if (PhotocopieConvertit > 20)
+            {
+                int PhotocopieTrois = PhotocopieConvertit - 20;
+                double Troisiemeprix = (PhotocopieTrois * 0.08) + 1.9;
+                Console.WriteLine("La facture est de " + Troisiemeprix);
+            }
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
