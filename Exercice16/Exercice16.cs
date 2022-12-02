656c427215669a37c448b112e057ad52dbb25fad
SylvainTorrenti <torrsyl@gmail.com>
2022-12-02 09:45:11 +0100

    exercice

---
+++ Exercice16/Exercice16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice16
+{
+    internal class Exercice16
+    {
+        static void Main(string[] args)
+        {
+            int Somme = 0; //Initialise la Somme à 0
+            int Nombre = 5; // Initialise le nombre à 5
+            while (Nombre > 0) // tant que le nombre > 0 alors
+            {
+                Somme = Nombre + Somme; // caclcule la somme avec la valeur du nombre actuel + la somme actuelle
+                Nombre = Nombre - 1; // enlêve 1 au nombrea actuel
+            }
+            Console.WriteLine("La somme est de " + Somme);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ Exercice16_1/Exercice16_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice16_1
+{
+    internal class Exercice16_1
+    {
+        static void Main(string[] args)
+        {
+            int Somme = 0;
+            Console.WriteLine("Entrez un nombre");
+            int Nombre = int.Parse(Console.ReadLine());
+            while (Nombre > 0)
+            {
+                Somme = Nombre + Somme;
+                Nombre = Nombre - 1;
+            }
+            Console.WriteLine("La somme est de " + Somme);
+            Console.ReadLine();
+        }
+    }
+}
