2f05ce61d762379d5ed1a933e39c711f99083fce
SylvainTorrenti <torrsyl@gmail.com>
2022-12-02 11:21:09 +0100

    exercice

---
+++ Exercice20/Exercice20.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice20
+{
+    internal class Exercice20
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Entrez un nombre");
+                int Nombre = int.Parse(Console.ReadLine());
+                int Somme = 0; //Initialise la Somme à 0
+                while (Nombre > 0) // tant que le nombre > 0 alors
+                {
+                    Somme = Nombre + Somme; // caclcule la somme avec la valeur du nombre actuel + la somme actuelle
+                    Nombre = Nombre - 1; // enlêve 1 au nombrea actuel
+                }
+                Console.WriteLine("La somme est de " + Somme);
+            }
+            
+                Console.WriteLine("Voulez vous fair un nouveau calcul? O/N");
+                string Reponse = Console.ReadLine();
+            
+
+            while (Reponse == "O")
+            {
+                Console.WriteLine("Entrez un nombre");
+                int Nombre = int.Parse(Console.ReadLine());
+                int Somme = 0; //Initialise la Somme à 0
+                while (Nombre > 0) // tant que le nombre > 0 alors
+                {
+                    Somme = Nombre + Somme; // caclcule la somme avec la valeur du nombre actuel + la somme actuelle
+                    Nombre = Nombre - 1; // enlêve 1 au nombrea actuel
+                }
+                {
+                    Console.WriteLine("La somme est de " + Somme);
+                    Console.WriteLine("Voulez vous fair un nouveau calcul? O/N");
+                    Reponse = Console.ReadLine();
+                }
+
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
