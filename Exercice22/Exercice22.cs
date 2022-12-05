6d481d93ef6326a71400bacfbe39fdb1d63b24f4
SylvainTorrenti <torrsyl@gmail.com>
2022-12-05 13:23:15 +0100

    exercice

--- Exercice22/Exercice22.cs
+++ Exercice22/Exercice22.cs
@@ -22,19 +22,19 @@ namespace Exercice22
                 int Modulo5 = Nombre % 5; // calcul le modulo 5 en fonction du nombre actuel
                 if ((Modulo3 == 0) && (Modulo5 == 0)) // Verifie si le nmodulo 3 et 5 du nombre = 0
                 {
-                    Console.WriteLine(FizzBuzz);
+                    Console.WriteLine(FizzBuzz); //si la condition se verifie ecrit FizzBuzz
                 }
                 else if (Modulo3 == 0) // sinon verifie si le modulo 3 = 0
                 {
-                    Console.WriteLine(Fizz);
+                    Console.WriteLine(Fizz); //si la condition se verifie ecrit Fizz
                 }
                 else if (Modulo5 == 0)// sinon verifie si le modulo 5 = 0 
                 {
-                    Console.WriteLine(Buzz);
+                    Console.WriteLine(Buzz); //si la condition se verifie ecrit Buzz
                 }
                 else if (!(Modulo3 == 0) && !(Modulo5 == 0)) // sinon verifie si le modulo 3 != 0 et le modulo 5 != 0
                 {
-                    Console.WriteLine(Nombre);
+                    Console.WriteLine(Nombre); //si la condition se verifie ecrit le nombre
                 }
                 Nombre = Nombre + 1;
             }
