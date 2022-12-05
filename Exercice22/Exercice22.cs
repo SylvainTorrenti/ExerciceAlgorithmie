36b2bf4f26ec9da8b9b2e622d27de5c71cc78fde
SylvainTorrenti <torrsyl@gmail.com>
2022-12-05 13:22:59 +0100

    exercice

--- Exercice22/Exercice22.cs
+++ Exercice22/Exercice22.cs
@@ -16,23 +16,23 @@ namespace Exercice22
             string FizzBuzz = "FizzBuzz ";
 
 
-            while (Nombre <= 100)
+            while (Nombre <= 100) // tant que Nombre est <= 100
             {
-                int Modulo3 = Nombre % 3;
-                int Modulo5 = Nombre % 5;
-                if ((Modulo3 == 0) && (Modulo5 == 0))
+                int Modulo3 = Nombre % 3; // calcul le modulo 3 en fonction du nombre actuel
+                int Modulo5 = Nombre % 5; // calcul le modulo 5 en fonction du nombre actuel
+                if ((Modulo3 == 0) && (Modulo5 == 0)) // Verifie si le nmodulo 3 et 5 du nombre = 0
                 {
                     Console.WriteLine(FizzBuzz);
                 }
-                else if (Modulo3 == 0)
+                else if (Modulo3 == 0) // sinon verifie si le modulo 3 = 0
                 {
                     Console.WriteLine(Fizz);
                 }
-                else if (Modulo5 == 0)
+                else if (Modulo5 == 0)// sinon verifie si le modulo 5 = 0 
                 {
                     Console.WriteLine(Buzz);
                 }
-                else if (!(Modulo3 == 0) && !(Modulo5 == 0))
+                else if (!(Modulo3 == 0) && !(Modulo5 == 0)) // sinon verifie si le modulo 3 != 0 et le modulo 5 != 0
                 {
                     Console.WriteLine(Nombre);
                 }
